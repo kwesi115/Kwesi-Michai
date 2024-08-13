0ab970d3a576d235a9b66e6ec539522b266cbfe6
kwesi115 <kwesimichai1@gmail.com>
2024-08-12 21:09:58 -0500

    first push

---
+++ script.js
@@ -0,0 +1,11 @@
+// Init TypedJs
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        strings: [ "a full stack Web Dev", "a Software Engineer", "a Freelancer", "a Blogger", "a Tech Enthusiast", "a Problem Solver", "a Learner" ],
+        typeSpeed: 150,
+        backSpeed: 100,
+        loop: true
+    };
+
+    var typed = new Typed("#typed", options);
+});
