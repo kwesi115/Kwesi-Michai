23cc08d3f7cb29fab2d186a9ba9d4e8d423493e8
kwesi115 <142618539+kwesi115@users.noreply.github.com>
2024-08-17 00:12:20 -0500

    e

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Init TypedJs
 document.addEventListener('DOMContentLoaded', function () {
     var options = {
-        strings: [ "a full stack Web Dev", "a Software Engineer", "a Freelancer", "a Blogger", "a Tech Enthusiast", "a Problem Solver", "a Learner" ],
+        strings: [ "a full stack Web Dev", "an Engineer", "a Freelancer", "a Blogger", "a Tech Enthusiast", "a Problem Solver", "a Learner" ],
         typeSpeed: 150,
         backSpeed: 100,
         loop: true
